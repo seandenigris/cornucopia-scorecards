a55e13fb48c224d2e5f5410cd9ef00deb0cb3d52
Sean DeNigris <sean@clipperadams.com>
2015-06-16 12:51:36 -0400

    Fix #3: Remove generated files from SCM

--- src/CornucopiaScorecards-Tests.js
+++
@@ -1,9 +0,0 @@
-define("amber-cornucopiascorecards/CornucopiaScorecards-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
-var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
-var smalltalk=$core,_st=$recv,globals=$globals;
-$core.addPackage('CornucopiaScorecards-Tests');
-$core.packages["CornucopiaScorecards-Tests"].transport = {"type":"amd","amdNamespace":"amber-cornucopiascorecards"};
-
-$core.addClass('CornucopiaScorecardsTest', $globals.TestCase, [], 'CornucopiaScorecards-Tests');
-
-});
