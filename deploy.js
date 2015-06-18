e50cddf7020ca5a4f95b362c30db70df11585b20
Sean DeNigris <sean@clipperadams.com>
2015-06-18 09:56:09 -0400

    Incorporate css into the.js

--- deploy.js
+++ deploy.js
@@ -1,5 +1,9 @@
 define([
     'amber/deploy',
+    'bootstrap/dist/js/bootstrap',
+    'css!amber-cornucopiascorecards/css/cover',
+    'css!amber-cornucopiascorecards/css/eggscores',
+    'css!//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui',
     // --- packages to be deployed begin here ---
     'amber-cornucopiascorecards/CornucopiaScorecardsApp',
     'amber-cornucopiascorecards/CornucopiaScorecards',
